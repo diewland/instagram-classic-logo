809d40abce4eb05029ee8b0bcfceb128c006dcaf
diewland <diewland@gmail.com>
2016-05-14 18:03:08 +0700

    handled no instagram case

--- app/src/main/java/com/diewland/instagram/MainActivity.java
+++ app/src/main/java/com/diewland/instagram/MainActivity.java
@@ -2,8 +2,9 @@ package com.diewland.instagram;
 
 import android.content.ComponentName;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent("android.intent.action.MAIN");
-        intent.setPackage("com.instagram.android");
-        intent.setFlags(0x10000000);
-        intent.setComponent(new ComponentName("com.instagram.android",
-                                              "com.instagram.android.activity.MainTabActivity"));
-        startActivityForResult(intent, 0);
+        try {
+
+            Intent intent = new Intent("android.intent.action.MAIN");
+            intent.setPackage("com.instagram.android");
+            intent.setFlags(0x10000000);
+            intent.setComponent(new ComponentName("com.instagram.android",
+                                                  "com.instagram.android.activity.MainTabActivity"));
+            startActivityForResult(intent, 0);
+        }
+        catch(Exception e){
+            Toast.makeText(this,
+                           "Please install official instagram first",
+                           Toast.LENGTH_LONG).show();
+            // go to play store
+            /*
+            String appPackageName = "com.instagram.android";
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+            } catch (android.content.ActivityNotFoundException anfe) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+            }
+            */
+        }
 
         finish();
     }
